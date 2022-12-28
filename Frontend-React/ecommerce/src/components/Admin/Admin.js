6d0c7ecb78b5eb8f7c451ba499d023222667de7c
vishvesh-hcl <vishveshkumar.patel@hcl.com>
2022-12-28 16:29:07 -0500

    Added security to display admin page if role is admin

--- Frontend-React/ecommerce/src/components/Admin/Admin.js
+++ Frontend-React/ecommerce/src/components/Admin/Admin.js
@@ -1,15 +1,19 @@
 import React, { useState } from "react";
-//import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import UserService from "../../services/user.service";
 
 export default function BoardAdmin() {
 
   const [content, setContent] = useState('');
+  const [roleAdmin, setRoleAdmin] = useState(false);
 
   React.useEffect(() => {
     UserService.getAdminBoard().then(
       response => {
-        setContent(response.data);
+        if(response.data === "Admin Board."){
+          setRoleAdmin(true)
+          setContent(response.data)
+        }
       },
       error => {
         setContent(
@@ -23,15 +27,15 @@ export default function BoardAdmin() {
     );
   }, []);
 
+
     return (
       <div className="container">
         <header className="jumbotron">
           <h3>{content}</h3>
         </header>
 
-        {/* working here */}
-
-        {/* <div className="navbar-nav mr-auto">
+        {roleAdmin && (   
+        <div className="navbar-nav mr-auto">
             <li className="nav-item">
               <Link to={"/admin/addproduct"} className="nav-link">
                 Add
@@ -42,7 +46,13 @@ export default function BoardAdmin() {
                 Product List
               </Link>
             </li>
-          </div> */}
+            <li className="nav-item">
+              <Link to={""} className="nav-link">
+                View users
+              </Link>
+            </li>
+          </div>
+          )}
       </div>
     );
 }
